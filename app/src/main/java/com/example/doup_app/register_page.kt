0544f3d91aadf1f668e4a35f6bef1deb58521abe
QBlind <Kevinsyah0203@gmail.com>
2023-01-02 15:03:48 +0700

    registerPAge
    lateInit

--- app/src/main/java/com/example/doup_app/register_page.kt
+++ app/src/main/java/com/example/doup_app/register_page.kt
@@ -3,9 +3,13 @@ package com.example.doup_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 
 class register_page : AppCompatActivity() {
     lateinit var btn: Button
+    lateinit var text: EditText
+    lateinit var rd: RadioButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
