84ddd417cb2ce6da289dfdcf52e2bb033986a5ec
QBlind <Kevinsyah0203@gmail.com>
2023-01-02 15:01:07 +0700

    RegisterPagee

--- app/src/main/java/com/example/doup_app/register_page.kt
+++ app/src/main/java/com/example/doup_app/register_page.kt
@@ -2,10 +2,13 @@ package com.example.doup_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class register_page : AppCompatActivity() {
+    lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
     }
 }
